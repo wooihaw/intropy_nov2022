8374dd8541013a6ae7e715e034e76b881c3bd768
wooihaw <wooihaw.tan@gmail.com>
2022-11-30 15:49:25 +0800

    Solutions added

--- exercises/02_list_exercises/01_second_largest.py
+++ exercises/02_list_exercises/01_second_largest.py
@@ -1,3 +1,9 @@
 # Write a Python script to find the second largest value in a list.
 alist = [12, 43, 57, 98, 83, -103, 256, -55, 185]
 print(f'{alist=}')
+
+print(f'Second largest number = {sorted(alist)[-2]}')
+
+# Alternative solution
+alist.sort()
+print(f'Second largest number = {alist[-2]}')
